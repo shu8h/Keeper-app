8c6593e952af5027f0ceec275eda547e4cf8e64c
shu8h <shbhkatyal@gmail.com>
2022-07-03 11:14:27 +0530

    Updates

---
+++ precache-manifest.4c3fa4ab13c989bdb870d7335a7f1530.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aca1dc5d011c969f16f57f6c9570933d",
+    "url": "/Keeper-app/index.html"
+  },
+  {
+    "revision": "a836dbac98a5aeb6e247",
+    "url": "/Keeper-app/static/js/2.ec16f9d9.chunk.js"
+  },
+  {
+    "revision": "4603776344231cdaf9e6",
+    "url": "/Keeper-app/static/js/main.3dbfabc3.chunk.js"
+  },
+  {
+    "revision": "6b292db5c8d07c9c8d18",
+    "url": "/Keeper-app/static/js/runtime-main.4ab04a6a.js"
+  }
+]);
